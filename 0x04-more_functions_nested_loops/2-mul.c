3d3aaefc4b6c86bef47f78164cdec37fd0227464
kbtisuu <kebi2393@gmail.com>
2023-02-23 23:08:50 +0300

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - function
+* @a: variable 1
+* @b: variable 2
+* Return: multiplication value
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Functions and nested loops are fundamental concepts in programming that are used to build complex programs.
